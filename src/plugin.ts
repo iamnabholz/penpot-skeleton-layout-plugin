d579fb49e9abc903fc3582f04cf2efaea1e131cc
Lukas Nabholz <lukas@nabholz.work>
2024-11-24 16:09:43 -0400

    Initial functionality

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,15 +1,56 @@
-penpot.ui.open("Penpot plugin starter template", `?theme=${penpot.theme}`);
+import { Group, Board, Shape } from "@penpot/plugin-types";
+
+penpot.ui.open("Skeleton Layout", `?theme=${penpot.theme}`, {
+  width: 240,
+  height: 260,
+});
+
+const createSkeleton = (element: Shape, parent: Board | Group) => {
+  const skeleton = penpot.createRectangle();
+  skeleton.borderRadius = 12;
+  skeleton.x = element.x;
+  skeleton.y = element.y;
+  skeleton.resize(element.width, element.height);
+  skeleton.fills = [{ fillOpacity: 0.6, fillColor: "#aeabab" }];
+
+  skeleton.name = element.name;
+
+  element.remove();
+
+  parent.insertChild(parent.children.length, skeleton);
+}
+
+const iterateChildren = (element: Group | Board) => {
+  const children = [...element.children];
+
+  children.forEach((child) => {
+    if (penpot.utils.types.isText(child)) {
+      child.growType = "auto-width";
+      createSkeleton(child, element);
+    } else if (penpot.utils.types.isRectangle(child) || penpot.utils.types.isEllipse(child) || penpot.utils.types.isPath(child)) {
+      child.fills = [{ fillOpacity: 0.6, fillColor: "#aeabab" }];
+    } else if (penpot.utils.types.isGroup(child) /*|| penpot.utils.types.isBoard(child)*/) {
+      iterateChildren(child);
+    } else if (penpot.utils.types.isBoard(child)) {
+      const grouped = penpot.group(child.children);
+      if (grouped) {
+        element.insertChild(element.children.length, grouped);
+        iterateChildren(grouped);
+        child.remove();
+      }
+    }
+  });
+}
 
 penpot.ui.onMessage<string>((message) => {
   if (message === "create-text") {
-    const text = penpot.createText("Hello world!");
+    const selection = penpot.selection;
 
-    if (text) {
-      text.x = penpot.viewport.center.x;
-      text.y = penpot.viewport.center.y;
-
-      penpot.selection = [text];
-    }
+    selection.forEach((board) => {
+      if (penpot.utils.types.isBoard(board)) {
+        iterateChildren(board);
+      }
+    });
   }
 });
 
